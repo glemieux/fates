ee5e02ba5dce5bb44d93b0775aca2124e8c142c9
Gregory Lemieux <glemieux@users.noreply.github.com>
2020-02-13 13:30:57 -0800

    forget to add variable definitions

--- biogeochem/EDPhysiologyMod.F90
+++ biogeochem/EDPhysiologyMod.F90
@@ -391,7 +391,11 @@ contains
     real(r8) :: lai_layers_above ! the LAI in the leaf layers, within the current canopy, 
                                  ! above the leaf layer of interest
     real(r8) :: lai_current      ! the LAI in the current leaf layer
-    real(r8) :: cumulative_lai(:)   ! the cumulative LAI, top down, to the leaf layer of interest
+    real(r8) :: cumulative_lai(nlevleaf)   ! the cumulative LAI, top down, to the leaf layer of interest
+
+    real(r8) :: cnv
+    real(r8) :: lai_nu_slope
+    real(r8) :: opt_cumulative_lai
 
     !----------------------------------------------------------------------
 
@@ -455,8 +459,9 @@ contains
 
          ! Calcuate the 'optimum' cumulative_lai to use for calculating the leaf cost
          cnv = currentCohort%nv 
-         lai_nu_slope = (cumulative_lai(cnv) - cumulative_lai(cnv-1)) / (year_net_uptake(cnv) - year_net_uptake(cnv-1))
-         opt_cumulative_lai = -lai_nu_slope * year_net_uptake(cnv) + cumulative_lai(cnv) 
+         lai_nu_slope = (cumulative_lai(cnv) - cumulative_lai(cnv-1)) / &
+                        (currentCohort%year_net_uptake(cnv) - currentCohort%year_net_uptake(cnv-1))
+         opt_cumulative_lai = -lai_nu_slope * currentCohort%year_net_uptake(cnv) + cumulative_lai(cnv) 
                    
          ! Calculate sla_levleaf following the sla profile with overlying leaf area
          ! Scale for leaf nitrogen profile
